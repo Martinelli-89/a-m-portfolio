609b0a632a502028813e8048af4c0603947a08a8
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-11-20 13:45:03 +0000

    added unmount menu animation

--- src/Components/MenuOption/MenuOption.jsx
+++ src/Components/MenuOption/MenuOption.jsx
@@ -1,11 +1,14 @@
 import "./MenuOption.scss";
 
-const MenuOption = ({text, delay}) => {
+const MenuOption = ({text, delay, startedUnmount, handleEndAnimation}) => {
 
     return (
-            <div className="text" style={{animationDelay:`${delay}s`}}>
+        <>
+            <div className={startedUnmount? "text text--out " : "text text--in"} style={{animationDelay:`${delay}s`}}>
                 <p>{text}</p>
             </div>
+            <div className={startedUnmount? "lineMenu line--out " : "lineMenu line--in"} style={{animationDelay:`${delay}s`}} onAnimationEnd={handleEndAnimation}></div>
+        </>
     );
 
 }
