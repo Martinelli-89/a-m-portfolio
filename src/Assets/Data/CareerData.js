cb3057b59c02d2a2af6dc7b563d9d51e10ae610c
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-11-21 09:52:47 +0000

    update career component

---
+++ src/Assets/Data/CareerData.js
@@ -0,0 +1,21 @@
+const career = {
+
+    Arnolfini: {
+        Venue : "Arnolfini Cafe Bar",
+        Role: "General Manager",
+        Text : "Managing a venue in central Bristol for the Bristol Beer Factory. The venue had a turnover of £1 million and I was responsible of a team of 25 people."
+    },
+    Architect: {
+        Venue : "The Architect",
+        Role: "Venue Manager",
+        Text : "Setting up and opening a new stylish independent venue in Central Bristol for Design West."
+    },
+    Gambas: {
+        Venue: "Gambas",
+        Role: "Part-time manager",
+        Text: "Supporting the GM and AM running a popular Bristol Restaurant. This was a part-time role while I took part in the _nology consultancy programme."
+    }
+
+}
+
+export default career;
--- src/Components/Career/Career.jsx
+++ src/Components/Career/Career.jsx
@@ -1,7 +1,32 @@
 import "./Career.scss";
 import flow from "../../Assets/Images/Flow2.svg";
+import career from "../../Assets/Data/CareerData";
+import { useState } from "react";
 
 const Career = () => {
+
+    const [venue, setVenue] = useState(career.Arnolfini.Venue);
+    const [role, setRole] = useState(career.Arnolfini.Role);
+    const [recap, setRecap] = useState(career.Arnolfini.Text);
+
+    const ArnolfiniClick = () => {
+        setVenue(career.Arnolfini.Venue);
+        setRole(career.Arnolfini.Role);
+        setRecap(career.Arnolfini.Text);
+    }
+
+    const ArchitectClick = () => {
+        setVenue(career.Architect.Venue);
+        setRole(career.Architect.Role);
+        setRecap(career.Architect.Text);
+    }
+
+    const GambasClick = () => {
+        setVenue(career.Gambas.Venue);
+        setRole(career.Gambas.Role);
+        setRecap(career.Gambas.Text);
+        console.log("fire");
+    }
 
     return (
 
@@ -9,12 +34,14 @@ const Career = () => {
             <section className="career">
                 <img src={flow} alt="git flow" className="career__chart"></img>
                 <div className="career__dates">
-                    <p>'17-'21</p>
-                    <p>'21-'22</p>
-                    <p>'22-Present</p>    
+                    <p onClick={ArnolfiniClick}>'17-'21</p>
+                    <p onClick={ArchitectClick}>'21-'22</p>
+                    <p onClick={GambasClick}>'22-Present</p>    
                 </div>
-                <div className="career__resume">
-                    <p>Resume here</p>
+                <div className="career__resume resume">
+                    <h5 className="resume__venue">{venue}</h5>
+                    <p className="resume__role">{role}</p>
+                    <p className="resume__text">{recap}</p>
                 </div>
             </section>
         </div>
