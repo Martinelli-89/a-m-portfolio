196fa6835090eb185465f568f3ecc1575840a7ac
Martinelli-89 <99336076+Martinelli-89@users.noreply.github.com>
2022-11-27 10:52:58 +0000

    adjusted cubes color

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import "./ContactForm.scss";
 
 const ContactForm = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleInput = (e) => {
+
+        switch(e.target.id) {
+            case("name"):
+                setName(e.target.value);
+                break;
+            case("email"):
+                setEmail(e.target.value);
+                break;
+            case("subject"):
+                setSubject(e.target.value);
+                break;
+            case("message"):
+                setMessage(e.target.value);
+                break;
+        }
+
+    }
 
     return (
 
@@ -8,19 +33,19 @@ const ContactForm = () => {
             <div className="contactForm">
                 <div className="contactForm__name">
                     <p>Name</p>
-                    <input></input>
+                    <input id="name" onChange={handleInput}></input>
                 </div>
                 <div className="contactForm__email">
                     <p>Email</p>
-                    <input></input>
+                    <input id="email" onChange={handleInput}></input>
                 </div>
                 <div className="contactForm__subject">
                     <p>Subject</p>
-                    <input></input>
+                    <input id="subject" onChange={handleInput}></input>
                 </div>
                 <div className="contactForm__message">
                     <p>Message</p>
-                    <textarea></textarea>
+                    <textarea id="message" onChange={handleInput}></textarea>
                 </div>
                 <div className="contactForm__send">Send</div>
             </div>
